187687a4622479f55a5d65386a7c520f5859f279
Cr3cker <61652620+Cr3cker@users.noreply.github.com>
2022-09-29 09:40:42 +0300

    Refactoring and adding new methods

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import random
 import math
 
+
+def get_readable_matrix(matrix):
+    new_matrix = []
+    for row in matrix:
+        print(row)
+    return '\n'.join(new_matrix)
+
+
 class Matrix:
     def __init__(self, x, y):
-        self.matrx = self.get_matrix(x, y)
+        self.x = x
+        self.y = y
+        self.matrx = self._get_matrix()
 
-    @staticmethod
-    def get_matrix(x, y):
-        matrix = [[i for i in range(y)] for _ in range(x)]
+    def _get_matrix(self):
+        matrix = [[i for i in range(self.y)] for _ in range(self.x)]
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 num = random.randint(1, 99)
                 matrix[i][j] = num
         return matrix
-
-    @staticmethod
-    def readable_matrix(matrix):
-        new_matrix = []
-        for row in matrix:
-            print(row)
-        return '\n'.join(new_matrix)
 
     def get_element(self, i, j):
         return self.matrx[i - 1][j - 1]
@@ -31,10 +33,10 @@ class Matrix:
     def transpose(self):
         transposed = [[self.matrx[j][i] for j in range(len(self.matrx))]
                       for i in range(len(self.matrx[0]))]
-        return self.readable_matrix(transposed)
+        return get_readable_matrix(transposed)
 
     def __str__(self):
-        return f"{self.readable_matrix(self.matrx)}"
+        return f"{get_readable_matrix(self.matrx)}"
 
     def __len__(self):
         return len(self.matrx)
@@ -42,18 +44,18 @@ class Matrix:
     def __getitem__(self, item):
         return self.matrx[item]
 
-    def multiply(self, matrix):
+    def __matmul__(self, matrix):
         result = [[0 for _ in range(len(matrix[i]))] for i in range(len(self.matrx))]
         for i in range(len(self.matrx)):
             for j in range(len(matrix[0])):
                 for k in range(len(matrix)):
                     result[i][j] += self.matrx[i][k] * matrix[k][j]
-        return self.readable_matrix(result)
+        return get_readable_matrix(result)
 
     def __mul__(self, scalar):
         if isinstance(scalar, Matrix):
-            return self.readable_matrix(self.multiply(scalar))
-        return self.readable_matrix([[num * scalar for num in row] for row in self.matrx])
+            return get_readable_matrix(self.__mul__(scalar))
+        return get_readable_matrix([[num * scalar for num in row] for row in self.matrx])
 
 
 class Vector:
@@ -77,5 +79,3 @@ class Vector:
 
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
-
-
